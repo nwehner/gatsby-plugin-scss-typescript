df878b28892b198aa5f446e895ae24afe3faae45
Nick Wehner <nwehner@users.noreply.github.com>
2021-05-24 08:20:10 -0700

    Remove bable and linting

--- test/serializer.ts
+++
@@ -1,6 +0,0 @@
-export const print = (value: string, serialize: (value: string) => string) => {
-    return serialize(value.replace(process.cwd(), ''));
-};
-
-export const test = (value: unknown) =>
-    typeof value === 'string' && value.includes(process.cwd());
